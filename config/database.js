60a3fd915b218fa05707a8010aaad78adbc88848
Anyi Lorena Gonzalez <lorena0118a@gmail.com>
2021-02-11 19:09:20 -0500

    credenciales clever-cloud

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mysql=require('mysql2')
 require('dotenv').config()
 
-const connectionmysql = mysql.createPool({
+const connectionmysql = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
